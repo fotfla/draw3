e744bafa5aa71ab17b30e7382a4f6cafaa759359
fotfla <fotfla.sg@gmail.com>
2023-08-05 17:56:36 +0900

    fix grid

--- Assets/Shader/Figure.hlsl
+++ Assets/Shader/Figure.hlsl
@@ -1,3 +1,15 @@
 void Figure_float(in float2 uv, in float aspect, in int kind, out float4 c){
     c = 1;
 }
+
+void Invert_float(in int kind, in float3 incolor, out float3 outcolor)
+{
+    if (kind > 0)
+    {
+        outcolor = 1 - incolor;
+    }
+    else
+    {
+        outcolor = incolor;
+    }
+}
